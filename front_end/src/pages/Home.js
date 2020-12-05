5e96b1c26d10f62fbe71fa11e1bd208a9f7f1ee6
Hannah <hannahhum643@gmail.com>
2020-12-04 22:58:19 -0800

    minor change to spacing

--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -8,22 +8,23 @@ function Home() {
       <h4>Welcome to The Turtle Ducks Website!</h4>
 
       <div class="grid">
+
         <div class="home-div">
           Home: <br />
-        Click Home to come back to this page!
+          Click Home to come back to this page!
       </div>
 
         <div class="home-div">
           Admin: <br />
-      Enter your listings~
-      <li>type</li>
+          Enter your listings~
+          <li>type</li>
           <li>price</li>
           <li>title</li>
         </div>
 
         <div class="home-div">
           Feed:<br />
-      Here, you will see all the available listings!
+          Here, you will see all the available listings!
       </div>
       </div>
     </div>
