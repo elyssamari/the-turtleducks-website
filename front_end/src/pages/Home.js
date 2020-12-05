b4a1639a069a64b8498b7e652a914cb52b9246f9
Hannah <hannahhum643@gmail.com>
2020-12-04 22:40:57 -0800

    added css to home page

--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -5,29 +5,29 @@ function Home() {
   return (
     <div>
       <h2>Home Page</h2>
-      <h3>Welcome to The Turtle Ducks Website!</h3>
+      <h4>Welcome to The Turtle Ducks Website!</h4>
 
-      <div id ="home-info">
-        Home: <br/>
+      <div class="grid">
+        <div class="home-div">
+          Home: <br />
         Click Home to come back to this page!
       </div>
 
-      <div id ="admin-info">
-      Admin: <br/>
+        <div class="home-div">
+          Admin: <br />
       Enter your listings~
       <li>type</li>
-      <li>price</li>
-      <li>title</li>
-      </div>
+          <li>price</li>
+          <li>title</li>
+        </div>
 
-      <div id ="feed-info">
-      Feed:<br/>
+        <div class="home-div">
+          Feed:<br />
       Here, you will see all the available listings!
       </div>
-
+      </div>
     </div>
 
-    
   );
 }
 
