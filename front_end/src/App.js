154c8b722f2c37fd72d021fb703170a41db76246
Elyssa Tapawan <70128989+elyssamari@users.noreply.github.com>
2020-12-08 20:12:33 -0800

    Added Turtleduck logo and picture + took out unnecessary code

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -6,20 +6,20 @@ import Home from './pages/Home';
 import './App.css';
 
 // React components
-export function getWebS(){
+export function getWebS() {
   return new WebSocket('ws://localhost:1234/ws');
 }
 function App() {
-  function refreshPage(){
+  function refreshPage() {
     window.location.reload();
   }
   return (
     <div className="App">
-      
       <nav>
-            <div onClick={refreshPage} class="alink"><Link to="/"><h3>Home</h3></Link></div>        
-            <div onClick={refreshPage} class="alink"><Link to="/admin"><h3>Admin</h3></Link></div>         
-            <div onClick={refreshPage} class="alink"><Link to="/feed"><h3>Feed</h3></Link></div>       
+      <img src="./Pictures/Duckie.png" alt="Duckie" width="165" height="135"></img>
+        <div onClick={refreshPage} class="alink"><Link to="/"><h3>Home</h3></Link></div>
+        <div onClick={refreshPage} class="alink"><Link to="/admin"><h3>Admin</h3></Link></div>
+        <div onClick={refreshPage} class="alink"><Link to="/feed"><h3>Feed</h3></Link></div>
       </nav>
       <Switch>
         <Route path="/admin">
--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -6,29 +6,28 @@ function Home() {
     <div>
       <h2>Home Page</h2>
       <h4>Welcome to The Turtle Ducks Website!</h4>
+      <img src="./Pictures/turtleduck.png" alt="TurtleDuck" width="330" height="230" id="TurtleDuck"></img>
 
       <div class="grid">
-
-        <div class="home-div" id ="home-box">
+          <div class="home-div" id="home-box">
           Home: <br />
           Click Home to come back to this page!
       </div>
 
-        <div class="home-div" id ="admin-box">
+      <div class="home-div" id="admin-box">
           Admin: <br />
           Enter your listings~
           <li>type</li>
           <li>price</li>
           <li>title</li>
-        </div>
+      </div>
 
-        <div class="home-div" id ="feed-box">
+      <div class="home-div" id="feed-box">
           Feed:<br />
           Here, you will see all the available listings!
-      </div>
+          </div>
       </div>
     </div>
-
   );
 }
 
